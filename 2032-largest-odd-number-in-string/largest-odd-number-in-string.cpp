e694be5ecf2d0ce424222204aaef0115ab620596
Navdeep223 <ns3321960@gmail.com>
2026-07-30 01:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.40%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
+        int idx=-1;
         for(int i=num.length()-1;i>=0;i--){
             if((num[i]-'0')%2==1){
-                return num.substr(0,i+1);
+                idx=i;
+                break;
             }
         }
+        if(idx==-1)
         return "";
+        int start=0;
+        while(start<idx && num[start]==0){
+            start++;
+        }
+        return num.substr(start,idx-start+1);
     }
 };
