916ed566ff8056adad5c3b1000efd47f1a766438
Navdeep223 <ns3321960@gmail.com>
2026-07-31 00:29:31 +0530

    Time: 84 ms (63.81%) | Memory: 350.1 MB (95.38%) - LeetSync

--- 4338-aggregate-two-time-series/aggregate-two-time-series.cpp
+++ 4338-aggregate-two-time-series/aggregate-two-time-series.cpp
@@ -3,47 +3,31 @@ public:
     vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1,
                                             vector<vector<int>>& series2) {
 
-        int i = series1.size() - 1;
-        int j = series2.size() - 1;
-
-        int nextValue1 = 0;
-        int nextValue2 = 0;
-
+        int i=series1.size()-1;
+        int j=series2.size()-1;
         vector<vector<int>> ans;
-
-        while (i >= 0 || j >= 0) {
-
-            if (j < 0 || (i >= 0 && series1[i][0] > series2[j][0])) {
-
-                // Timestamp exists only in series1
-                ans.push_back({series1[i][0], series1[i][1] + nextValue2});
-
-                nextValue1 = series1[i][1];
+        int value1=0;
+        int value2=0;
+        while(i>=0 || j>=0){
+            if(j<0 || (i>=0 && series1[i][0] > series2[j][0])){
+                ans.push_back({series1[i][0],series1[i][1]+value2});
+                value1=series1[i][1];
                 i--;
             }
-            else if (i < 0 || (j >= 0 && series2[j][0] > series1[i][0])) {
-
-                // Timestamp exists only in series2
-                ans.push_back({series2[j][0], nextValue1 + series2[j][1]});
-
-                nextValue2 = series2[j][1];
+            else if(i<0 || (j>=0 && series2[j][0] > series1[i][0])){
+                ans.push_back({series2[j][0], series2[j][1]+value1});
+                value2=series2[j][1];
                 j--;
             }
-            else {
-
-                // Same timestamp in both series
-                ans.push_back(
-                    {series1[i][0], series1[i][1] + series2[j][1]});
-
-                nextValue1 = series1[i][1];
-                nextValue2 = series2[j][1];
-
+            else{
+                ans.push_back({series1[i][0],series1[i][1]+series2[j][1]});
+                value1=series1[i][1];
+                value2=series2[j][1];
                 i--;
                 j--;
             }
         }
-
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
