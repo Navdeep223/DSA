a40272e02c10f126ef3f506a91d582f0d62f63df
Navdeep223 <ns3321960@gmail.com>
2026-07-31 14:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (82.83%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+ void allsubs(int index, vector<int> &nums, vector<int> &subset, vector<vector<int>> &ans){
+    if(index==nums.size()){
+        ans.push_back(subset);
+        return;
+    }
+    subset.push_back(nums[index]);
+    allsubs(index+1,nums,subset,ans);
+    subset.pop_back();
+    allsubs(index+1,nums,subset,ans);
+        }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        allsubs(0,nums,subset,ans);
+        return ans;
+    }
+};
