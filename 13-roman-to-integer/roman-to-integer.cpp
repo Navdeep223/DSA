5177ca5c774bb8078573987f449f54df4d5d8ca0
Navdeep223 <ns3321960@gmail.com>
2026-08-05 01:34:41 +0530

    Time: 8 ms (42.01%) | Memory: 13.3 MB (52.21%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int romanToInt(string s) {
-         unordered_map<char, int> roman = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        unordered_map<char,int> roman={
+            {'I',1},{'V',5},{'X',10},
+            {'L',50},{'C',100},{'D',500},{'M',1000}
         };
-        int res=0;
-        // Iterate through the string, except the last character
-        for (int i = 0; i < s.size() - 1; i++) {
-            // Subtract if current numeral is less than the next
-            if (roman[s[i]] < roman[s[i + 1]]) {
-                res -= roman[s[i]];
-            } else {
-                // Otherwise, add the current value
-                res += roman[s[i]];
-            }
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(i+1<s.size() && roman[s[i]]<roman[s[i+1]])
+            ans-=roman[s[i]];
+            else 
+            ans+=roman[s[i]];
         }
-
-        // Add the value of the last character
-        return res + roman[s.back()];
+        return ans;
     }
 };
